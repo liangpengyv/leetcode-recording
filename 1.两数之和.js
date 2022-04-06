f98da1bd2edd6069550aac97770bcaddaea4ecc8
liangpengyv <mrliangpengyv@gmail.com>
2022-04-06 23:31:17 +0800

    更新题目

---
+++ 1.两数之和.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i]
+    }
+    map.set(nums[i], i)
+  }
+  return []
+}
+// @lc code=end
+
+const nums = [2, 7, 11, 15]
+const target = 9
+console.log(twoSum(nums, target))
---
+++ README.md
@@ -0,0 +1,3 @@
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+# leetcode-recording

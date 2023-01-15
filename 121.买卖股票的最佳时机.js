a2b3256281ac7400e8aaf1939a3d7ceaa5669e66
liangpengyv <me@laoliang.ink>
2023-01-15 20:45:35 +0800

    更新题目

---
+++ 121.买卖股票的最佳时机.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=javascript
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function (prices) {
+  let minPrice = Infinity
+  let maxProfit = -Infinity
+  for (let i = 0; i < prices.length; i++) {
+    minPrice = Math.min(minPrice, prices[i])
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice)
+  }
+  return maxProfit > 0 ? maxProfit : 0
+}
+// @lc code=end

e71eb03ba1929fb59c0f66af67193a03cdbbb8cb
梁鹏宇 <me@laoliang.ink>
2026-07-19 12:07:05 +0800

    refactor: 更新题解 605

--- src/605.种花问题.js
+++ src/605.种花问题.js
@@ -11,17 +11,32 @@
  * @return {boolean}
  */
 const canPlaceFlowers = function (flowerbed, n) {
-  let canUse = 0
+  let count = 0
 
-  for (let i = 0; i < flowerbed.length; i++) {
-    if (!flowerbed[i - 1] && !flowerbed[i] && !flowerbed[i + 1]) {
+  // [0]
+  if (flowerbed.length === 1 && flowerbed[0] === 0) {
+    return n <= 1
+  }
+
+  // [0, 0, ...]
+  if (flowerbed[0] === 0 && flowerbed[1] === 0) {
+    flowerbed[0] = 1
+    count++
+  }
+
+  for (let i = 1; i < flowerbed.length - 1; i++) {
+    if (flowerbed[i] === 0 && flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0) {
       flowerbed[i] = 1
-      canUse++
+      count++
+      i++
     }
   }
 
-  return n <= canUse
+  // [..., 0, 0]
+  if (flowerbed[flowerbed.length - 1] === 0 && flowerbed[flowerbed.length - 2] === 0) {
+    count++
+  }
+
+  return count >= n
 }
 // @lc code=end
-
-console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2))
